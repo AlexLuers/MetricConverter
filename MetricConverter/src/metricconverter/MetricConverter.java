ca9f85e8f718171a0a44ebc2df5b47a3e723ad9f
AlexLuers <156149375+AlexLuers@users.noreply.github.com>
2024-02-22 12:48:36 -0500

    Update MetricConverter.java
    For some reason, the code didn't carry over on this one like it did for the other ones. I fixed it.

--- MetricConverter/src/metricconverter/MetricConverter.java
+++ MetricConverter/src/metricconverter/MetricConverter.java
@@ -14,7 +14,28 @@ public class MetricConverter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Scanner in = new Scanner(System.in);
+        
+        double meters = 0;
+        double miles;
+        double feet;
+        double inches;
+        
+        System.out.println("What is the your distance in meters?");
+        if (in.hasNextDouble())
+        {
+            meters = in.nextDouble();
+        }
+        else
+        {
+            System.out.println("That is not a valid input!");
+        }
+        miles = meters / 1609.344;
+        System.out.println("That diatsnce in miles is: " + miles);
+        feet = meters / 3.2808399;
+        System.out.println("That distance in feet is: " + feet);
+        inches = meters / 39.3700787;
+        System.out.println("That distance in inches is + inches");
     }
     
 }
